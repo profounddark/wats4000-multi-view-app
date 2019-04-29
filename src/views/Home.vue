067599ac20763234efe5b1634b47c5f870a9bc6d
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-04-28 17:09:34 -0700

    FIrst commit, after finishing the form.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="home">
-    <div class="form-container"><!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
+    <div class="form-container" v-show="showForm">
       <h1>Join the Web Developers Club!</h1>
       <p>Sign up to access our special, secret page. Just create an account and answer a brief survey.</p>
 
-      <!-- TODO: Add an HTML element to display an error message for when the user submits invalid information. Use v-show to show/hide this message based on the validity of the form data. -->
+      <p class="error" v-show="showError">Please check the information you have entered. Be sure to fill in all fields.</p>
 
-      <form><!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
+      <!-- Added autocomplete attributes to the form. -->
+      <form v-on:submit.prevent="validateForm" autocomplete="on">
+        <p>
+          <label for="username">Username
+            <input type="text" id="username" autocomplete="username" v-model="username">
+          </label>
+        </p>
 
-        <!-- TODO: Add labels for each form field in this form. -->
+        <p>
+          <label for="email">Email
+            <input type="email" id="email" autocomplete="email" v-model="email">
+          </label>
+        </p>
 
-        <!-- TODO: Add a username field. -->
+        <p>
+          <label for="password">Password
+            <input type="password" id="password" autocomplete="new-password" v-model="password">
+          </label>
+        </p>
 
-        <!-- TODO: Add an email field. -->
-
-        <!-- TODO: Add a password field. -->
-
-        <!-- TODO: Add a passwordVerify field. -->
-
-
+        <p>
+          <label for="passwordVerify">Verify Password
+            <input type="password" id="passwordVerify" autocomplete="new-password" v-model="passwordVerify">
+          </label>
+        </p>
+        
         <p><input type="submit" value="Submit"></p>
       </form>
     </div>
-    <div class="success-message"><!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
+    <div class="success-message" v-show="!showForm"><!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
       <h1>Thank you for signing up!</h1>
       <p>Please take our new member survey. Click here</p><!-- TODO: Link "Click here" to the survey page. -->
     </div>
@@ -44,14 +57,14 @@ export default {
   },
   methods: {
     validateForm: function () {
-      // Validate the form by checking the following values:
-      // username must not be blank
-      // email must not be blank
-      // password and passwordVerify must be equal
-      //
-      // When the form is validated, show the .success-message content
-      // If the form is invalid, show the form error message
-
+      if ((this.username != '') &&
+          (this.email != '') &&
+          (this.password != '') &&
+          (this.password === this.passwordVerify)){
+            this.showForm = false;
+          } else {
+            this.showError = true;
+          }
     }
   }
 }
